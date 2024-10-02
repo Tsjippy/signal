80ce59bba44cde1fb6da46fe2198d89278a3bcc9
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2024-10-02 08:56:06 +0100

    Update update_version.py

--- .github/workflows/update_version.py
+++ .github/workflows/update_version.py
@@ -12,7 +12,7 @@ for x in p.iterdir():
 dirPath = str(pathlib.Path(__file__).parent.parent.parent)
 
 print(dirPath)
-for x in Path(__file__+'/../../../php').iterdir():
+for x in Path(dirPath + '/php').iterdir():
     print(x)
 
 name    = dirPath+'/php/_module_menu.php'
