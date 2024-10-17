692d307e25e84827b3f51198cb62a7d2231ce9d6
Tsjippy <ewaldha@hotmail.com>
2024-10-17 13:12:32 +0100

    changed: block

--- blocks/signal_options/build/index.asset.php
+++ blocks/signal_options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-plugins'), 'version' => 'aa76395ef59311bfd32c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-plugins'), 'version' => '24ac32e833c2b2c8cc18');
