dec65cac03e99c474d91c4255d9ffdf1e7f040fc
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-08-17 11:58:45 +0100

    Update library.php

--- php/library.php
+++ php/library.php
@@ -6,7 +6,7 @@ add_action('sim-library-send-book-of-the-day', __NAMESPACE_.'\sendBook', 10, 3);
 function sendBook($message, $image, $url){
     $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
 
-    $message = str_replace('<br>', "\n\n", $message);
+    $message = str_replace('<br>', "\n", $message);
     $message = $message . "\n\nFind it here:$url";
     
     foreach($recipients as $recipient){
