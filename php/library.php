de4dfa7780032892430d0b5cff9d837120e6fc11
Tsjippy <ewaldha@hotmail.com>
2025-08-18 08:39:46 +0100

    changed: book of the day message

--- php/library.php
+++ php/library.php
@@ -3,13 +3,13 @@ namespace SIM\SIGNAL;
 use SIM;
 
 add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 3);
-function sendBook($message, $image, $url){
-    $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
+function sendBook($description, $title, $image, $url){
+  $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
 
-    $message = str_replace('<br>', "\n", $message);
-    $message = $message . "\n\nFind it here:$url";
-    
-    foreach($recipients as $recipient){
-		sendSignalMessage($message, $recipient, $image);
-	}
+  $description = str_replace('<br>', "\n", $description);
+  $message = "Book of the day: <b>$title</b>\n\n$description\n\nFind it here:$url";
+
+  foreach($recipients as $recipient){
+    sendSignalMessage($message, $recipient, $image);
+  }
 }
