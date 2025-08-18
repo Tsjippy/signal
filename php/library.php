55ab8369a7b616b1c1a8c1cb79681d1bb6ea1419
Tsjippy <ewaldha@hotmail.com>
2025-08-18 08:34:57 +0100

    added: send book of the day

--- php/library.php
+++ php/library.php
@@ -2,7 +2,7 @@
 namespace SIM\SIGNAL;
 use SIM;
 
-add_action('sim-library-send-book-of-the-day', __NAMESPACE_.'\sendBook', 10, 3);
+add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 3);
 function sendBook($message, $image, $url){
     $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
 
