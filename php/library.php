11c39da07ec3940950d658f304a5205556beee91
Tsjippy <ewaldha@hotmail.com>
2025-08-18 09:26:14 +0100

    fixed: bug in book signal message

--- php/library.php
+++ php/library.php
@@ -2,11 +2,10 @@
 namespace SIM\SIGNAL;
 use SIM;
 
-add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 3);
+add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 4);
 function sendBook($description, $title, $image, $url){
   $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
 
-  $description = str_replace('<br>', "\n", $description);
   $message = "Book of the day: <b>$title</b>\n\n$description\n\nFind it here:$url";
 
   foreach($recipients as $recipient){
