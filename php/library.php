47f4ab1815db7031c70463e542de3fae38329c2c
Tsjippy <ewaldha@hotmail.com>
2025-08-18 11:08:31 +0100

    changed: book of the day message title on seperate line

--- php/library.php
+++ php/library.php
@@ -6,7 +6,7 @@ add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 4)
 function sendBook($description, $title, $image, $url){
   $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
 
-  $message = "Book of the day: <b>$title</b>\n\n$description\n\nFind it here:$url";
+  $message = "Book of the day:\n\n<b>$title</b>\n\n$description\n\nFind it here:\nhttps:\\\\$url";
 
   foreach($recipients as $recipient){
     sendSignalMessage($message, $recipient, $image);
