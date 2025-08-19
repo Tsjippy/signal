bc8d1a47dc3462c729bfc3fee56062ca6c7b1f2b
Tsjippy <ewaldha@hotmail.com>
2025-08-19 13:39:53 +0100

    fixed: send book picture

--- php/library.php
+++ php/library.php
@@ -2,13 +2,17 @@
 namespace SIM\SIGNAL;
 use SIM;
 
-add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 4);
-function sendBook($description, $title, $image, $url){
+add_action('sim-library-send-book-of-the-day', __NAMESPACE__.'\sendBook', 10, 5);
+function sendBook($description, $title, $image, $url, $locations){
   $recipients		= SIM\getModuleOption(MODULE_SLUG, 'groups');
 
-  $message = "Book of the day:\n\n<b>$title</b>\n\n$description\n\nFind it here:\nhttps:\\\\$url";
+  $excerptMore 		= apply_filters('excerpt_more', ' [...]');
+
+  $description    = str_replace($excerptMore, '', $description);
+
+  $message = "Book of the day:\n\n<b>$title</b>\n\n$description\n\nRead more on:\n$url\n\nFind the book in the library at ".implode(' & ', $locations);
 
   foreach($recipients as $recipient){
-    sendSignalMessage($message, $recipient, $image);
+    sendSignalMessage($message, $recipient, [$image]);
   }
 }
