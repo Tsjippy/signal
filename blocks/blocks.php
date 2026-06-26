7d57bcb1f085c1b53c7e708082bdcb78282ec0b6
Tsjippy <ewaldha@hotmail.com>
2026-06-26 11:01:02 +0200

    fixed: non-prefixd post meta's

--- blocks/blocks.php
+++ blocks/blocks.php
@@ -20,31 +20,31 @@ function loadBlockAssets()
 add_action('init', __NAMESPACE__ . '\blockInit');
 function blockInit()
 {
-    register_post_meta('', 'send_signal', array(
-        'show_in_rest'         => true,
-        'single'             => true,
-        'type'                 => 'boolean',
+    register_post_meta('', "tsjippy_send_signal", array(
+        'show_in_rest'      => true,
+        'single'            => true,
+        'type'              => 'boolean',
         'sanitize_callback' => 'sanitize_text_field'
     ));
 
-    register_post_meta('', 'signal_message_type', array(
-        'show_in_rest'         => true,
-        'single'             => true,
-        'type'                 => 'string',
+    register_post_meta('', "tsjippy_signal_message_type", array(
+        'show_in_rest'      => true,
+        'single'            => true,
+        'type'              => 'string',
         'sanitize_callback' => 'sanitize_text_field'
     ));
 
-    register_post_meta('', 'signal_extra_message', array(
-        'show_in_rest'         => true,
-        'single'             => true,
-        'type'                 => 'string',
+    register_post_meta('', "tsjippy_signal_extra_message", array(
+        'show_in_rest'      => true,
+        'single'            => true,
+        'type'              => 'string',
         'sanitize_callback' => 'sanitize_text_field'
     ));
 
-    register_post_meta('', 'signal_url', array(
-        'show_in_rest'         => true,
-        'single'             => true,
-        'type'                 => 'boolean',
+    register_post_meta('', "tsjippy_signal_url", array(
+        'show_in_rest'      => true,
+        'single'            => true,
+        'type'              => 'boolean',
         'sanitize_callback' => 'sanitize_text_field'
     ));
 }
