9b92604855715ae1ea5a6360e32302bb95623652
Tsjippy <ewaldha@hotmail.com>
2026-09-20 16:52:30 +0200

    changed: load minification js depending on SCRIPT_DEBUG constant

--- php/enqueue.php
+++ php/enqueue.php
@@ -7,8 +7,8 @@ use TSJIPPY;
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\loadAssets');
 function loadAssets()
 {
-    wp_register_script_module('@tsjippy/signal_options', TSJIPPY\pathToUrl(PLUGINPATH . 'js/signal.min.js'), array('@tsjippy/formsubmit_script'), PLUGINVERSION);
-    wp_register_script_module('@tsjippy/signal_admin', TSJIPPY\pathToUrl(PLUGINPATH . 'js/admin.min.js'), array('@tsjippy/formsubmit_script'), PLUGINVERSION);
+    wp_register_script_module('@tsjippy/signal_options', TSJIPPY\pathToUrl(PLUGINPATH . 'js/signal' . TSJIPPY\JSEXTENSION), array('@tsjippy/formsubmit_script'), PLUGINVERSION);
+    wp_register_script_module('@tsjippy/signal_admin', TSJIPPY\pathToUrl(PLUGINPATH . 'js/admin' . TSJIPPY\JSEXTENSION), array('@tsjippy/formsubmit_script'), PLUGINVERSION);
 }
 
 add_action('admin_enqueue_scripts', __NAMESPACE__ . '\loadAdminAssets');
@@ -19,5 +19,5 @@ function loadAdminAssets($hook)
         return;
     }
 
-    wp_enqueue_script_module('@tsjippy/signal_admin', TSJIPPY\pathToUrl(PLUGINPATH . 'js/admin.min.js'), array('@tsjippy/main'), PLUGINVERSION);
+    wp_enqueue_script_module('@tsjippy/signal_admin', TSJIPPY\pathToUrl(PLUGINPATH . 'js/admin' . TSJIPPY\JSEXTENSION), array('@tsjippy/main'), PLUGINVERSION);
 }
